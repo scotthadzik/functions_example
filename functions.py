6eca16c8a055857cf3dab27fd58f4b6e45ec5ce3
scotthadzik <scotthadzik@weber.edu>
2022-11-15 18:58:22 -0700

    get user number

---
+++ functions.py
@@ -0,0 +1,10 @@
+# Conversion Calculator
+# By: Scott Hadzik
+
+# user input regarding the length to convert
+# get the unit from the user
+# convert the length to the correct unit
+# output the answer to the user
+
+user_number = input("What number to convert? ")
+print(user_number)
